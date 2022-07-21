e1012f91029c0798357fccf9f8de8857e9572cd4
Firdha Roofi Irawan <roofiii96@gmail.com>
2022-07-21 18:40:28 +0700

    update3

--- src/component/Movie.jsx
+++ src/component/Movie.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { FaHeart, FaRegHeart } from "react-icons/fa";
 
 const Movie = ({ item }) => {
-  const [like, setLike] = useState(false);
+  const [like] = useState(false);
   return (
     <div className="w-[160px] sm:w-[200px] md:w-[240px] lg:w-[280px] inline-block cursor-pointer relative p-2">
       <img
